d7460abedbe9816e768567fa7c07f0bd6e63f4ce
bomangoo <mocona000@naver.com>
2020-06-04 18:18:24 +0900

    setState 콜백함수 만들기

--- bomi/src/Counter.js
+++ bomi/src/Counter.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    constructor(props){
-        super(props);
-        // state 초깃값 설정할 때 호출해야 하는 컴포넌트 생성자 메서드
-        this.state = {
-            number : 0
-        };
-    }
+    state = {
+        number : 0,
+        fixedNumber : 0
+    };
     render(){
-        const {number} = this.state;
+        const {number, fixedNumber} = this.state;
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button onClick = {() => {
-                    this.setState({number : number + 1});
+                    this.setState(
+                        {
+                            number: number + 1
+                        },
+                        () => {
+                            console.log('방금 setState가 호출되었습니다.');
+                            console.log(this.state);
+                        }
+                    );
                 }}
                 >
                     +1
